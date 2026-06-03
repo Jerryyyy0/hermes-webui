11ecfd501b8cfd9624f934fb317b153b4f462eb0
Lover <w616902676@163.com>
2026-06-03 15:25:17 +0800

    更新 git.sh 脚本中的合并命令，将 dev 分支更改为 dev0530，以确保合并正确的开发版本。

--- git.sh
+++ git.sh
@@ -11,9 +11,15 @@ git fetch upstream
 git merge --ff-only upstream/master
 
 # 5. 再合并 dev
-git merge dev
+git merge dev0530
 
 如果第 4 步 --ff-only 失败，说明你的 master 已经有不同于上游的提交，不要硬来，先看：
 git log --oneline --graph --decorate --all -20
 git log --oneline upstream/master..master
 git log --oneline master..upstream/master
+
+
+
+git remote add gitlab ssh://git@192.168.1.139:2222/sgitg_x/hermes-webui.git
+
+git push -u gitlab HEAD:main
